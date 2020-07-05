aa5b3a7a5141cd639839ef38118a6d28e9989889
Ravindra Patidar <ravindra.patidar@oracle.com>
2020-07-05 10:05:19 +0530

    Api for ActivateMessageService return response is strign with Yes and No

---
+++ SanetizeMe/app/src/main/java/com/sanitize/sanitizeme/ActivateMessage.java
@@ -0,0 +1,21 @@
+package com.sanitize.sanitizeme;
+
+public class ActivateMessage {
+    private String ActivateMessage;
+
+    public String getActivateMessage() {
+        return ActivateMessage;
+    }
+
+    public void setActivateMessage(String activateMessage) {
+        ActivateMessage = activateMessage;
+    }
+
+
+    public ActivateMessage(String activateMessage) {
+        ActivateMessage = activateMessage;
+    }
+
+
+
+}
